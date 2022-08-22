8e0f9a1bb36e6d892d1a9adc2b0bfb1501992ec0
KingTune <tunechikalifa365@gmail.com>
2022-08-22 06:27:49 -0700

    cp

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -1,73 +1,56 @@
-#include "main.h"
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <unistd.h>
 #include <stdio.h>
+#include <fcntl.h>
+#include <stdlib.h>
 
 /**
- * error_file - checks if files can be opened.
- * @file_from: file_from.
- * @file_to: file_to.
- * @argv: arguments vector.
- * Return: no return.
+ * main - checks my code :)
+ *
+ * @ac: argument count
+ * @av: argument vector
+ * Return: Always 0
  */
-void error_file(int file_from, int file_to, char *argv[])
+
+int main(int ac, char **av)
 {
-	if (file_from == -1)
-	{
-		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]);
-		exit(98);
-	}
-	if (file_to == -1)
-	{
-		dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]);
-		exit(99);
-	}
-}
+	int fd, fd2;
+	int i, j, k, l;
+	char buff[1024];
 
-/**
- * main - check the code for Holberton School students.
- * @argc: number of arguments.
- * @argv: arguments vector.
- * Return: Always 0.
- */
-int main(int argc, char *argv[])
-{
-	int file_from, file_to, err_close;
-	ssize_t nchars, nwr;
-	char buf[1024];
-
-	if (argc != 3)
+	if (ac != 3)
+		dprintf(STDERR_FILENO, "Usage: cp file_from file_to\n"),
+			exit(97);
+	fd = open(av[1], O_RDONLY);
+	if (fd == -1)
+		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n",
+			av[1]), exit(98);
+	fd2 = open(av[2], O_CREAT | O_WRONLY | O_TRUNC, 0664);
+	if (fd2 == -1)
+		dprintf(STDERR_FILENO, "Error: Can't write to %s\n",
+			av[2]), exit(99);
+	while ((i = read(fd, buff, 1024)) > 0)
 	{
-		dprintf(STDERR_FILENO, "%s\n", "Usage: cp file_from file_to");
-		exit(97);
+		j = write(fd2, buff, i);
+		if (j != i)
+		{
+			dprintf(STDERR_FILENO, "Error: Can't write to %s\n"
+				, av[2]), exit(99);
+		}
 	}
+	if (i == -1)
+		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n",
+			av[1]), exit(98);
 
-	file_from = open(argv[1], O_RDONLY);
-	file_to = open(argv[2], O_CREAT | O_WRONLY | O_TRUNC | O_APPEND, 0664);
-	error_file(file_from, file_to, argv);
+	k = close(fd);
+	if (k == -1)
+		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n",
+			fd), exit(100);
+	l = close(fd2);
+	if (l == -1)
+		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n",
+			fd2), exit(100);
 
-	nchars = 1024;
-	while (nchars == 1024)
-	{
-		nchars = read(file_from, buf, 1024);
-		if (nchars == -1)
-			error_file(-1, 0, argv);
-		nwr = write(file_to, buf, nchars);
-		if (nwr == -1)
-			error_file(0, -1, argv);
-	}
-
-	err_close = close(file_from);
-	if (err_close == -1)
-	{
-		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", file_from);
-		exit(100);
-	}
-
-	err_close = close(file_to);
-	if (err_close == -1)
-	{
-		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", file_from);
-		exit(100);
-	}
 	return (0);
 }
-
